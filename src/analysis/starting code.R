50e7fbe2c49bb320ee247304cfe217a6ef887f4f
WiebeWeijs <123948939+WiebeWeijs@users.noreply.github.com>
2023-02-27 13:44:16 +0100

    Update starting code.R

--- src/analysis/starting code.R
+++ src/analysis/starting code.R
@@ -1,5 +1,5 @@
 install.packages("readr")
 library("readr")
 
-download.file("https://raw.githubusercontent.com/course-dprep/team-assignment-team3/master/listings.csv", destfile = "listings.csv")
+download.file("https://raw.githubusercontent.com/course-dprep/team-assignment-team3/master/data/dataset1/listings.csv", destfile = "listings.csv")
 Listings <- read.csv("listings.csv")
