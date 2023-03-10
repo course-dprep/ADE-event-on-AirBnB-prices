bf3e3405b4be345abe722201b991fff8da14de8a
Emi <112580804+Emi676@users.noreply.github.com>
2023-03-10 14:10:53 +0100

    Update calendar.data.R

--- src/data-preparation/calendar.data.R
+++ src/data-preparation/calendar.data.R
@@ -30,5 +30,34 @@ data <- rbind(data1, data2, data3, data4)
 # View the merged data frame
 View(data)
 
+# download and merge csv files Amsterdam 2022
+df1 <- read.csv("http://data.insideairbnb.com/the-netherlands/north-holland/amsterdam/2022-12-05/visualisations/listings.csv")
+df2 <- read.csv("http://data.insideairbnb.com/the-netherlands/north-holland/amsterdam/2022-09-07/visualisations/listings.csv")
+df3 <- read.csv("http://data.insideairbnb.com/the-netherlands/north-holland/amsterdam/2022-06-05/visualisations/listings.csv")
+df4 <- read.csv("http://data.insideairbnb.com/the-netherlands/north-holland/amsterdam/2022-03-08/visualisations/listings.csv")
+
+df_a <- rbind(df1,df2)
+df_b <- rbind(df3,df4)
+df <- rbind(df_a, df_b)
+
+
+updated_df <- select(df, -name, -host_id, -host_name, -neighbourhood_group, -latitude, - longitude, -number_of_reviews, -last_review, - reviews_per_month, -calculated_host_listings_count, -availability_365, -number_of_reviews_ltm, -license)
+
+
+data_filtered <- filter(data, year(ymd(date)) == 2022)
+
+
+#remove column " available"
+data_filtered2 <- select(data_filtered, -available)
+
+
+# joined Updated_df and data_filtered2
+joined_data <- updated_df %>% 
+  inner_join(data_filtered2, by = c("id"="listing_id"), suffix = c("_updated_df", "_data_filtered2"), multiple = "all")
+
+
+# remove duplicated from joined_data
+ deduplicated_joined_data <- distinct( joined_data)
+
+
 #write the data frame "data" to a specified file path and name in CSV format.
-write_csv(data, "data.csv")
