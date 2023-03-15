78d1da0690d4a0414981c4f7f52a8f19ea25b827
WiebeWeijs <123948939+WiebeWeijs@users.noreply.github.com>
2023-03-15 18:25:45 +0100

    regression analysis

--- src/data-preparation/regression.R
+++
@@ -1,8 +0,0 @@
-# Load the "stats" package
-library(stats)
-
-# Perform the multiple regression analysis
-lr_model <- lm(avg_price ~ dummy + room_type, data = grouped_df2)
-
-# Print the summary of the regression model
-summary(lr_model)
