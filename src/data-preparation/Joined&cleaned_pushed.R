824d7684c8c70de5250529c841f96ebf4546c2c3
WiebeWeijs <123948939+WiebeWeijs@users.noreply.github.com>
2023-03-15 17:54:11 +0100

    updated with write.csv

--- src/data-preparation/Joined&cleaned_pushed.R
+++ src/data-preparation/Joined&cleaned_pushed.R
@@ -76,3 +76,14 @@ if (any(duplicates)) {
 }
 
 
+#make dummmy for dates during ADE (=1) and not (=0)
+grouped_df2$dummy <- ifelse(grouped_df2$date >= as.Date("2022-10-19") & grouped_df2$date <= as.Date("2022-10-23"), 1, 0)
+
+#print
+print(grouped_df2)
+head(grouped_df2)
+
+#output
+write.csv(grouped_df2, "grouped_df2.csv", row.names = FALSE)
+
+
