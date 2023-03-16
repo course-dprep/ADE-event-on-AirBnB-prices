4812c19c7378abffa246a59a5f56446c6b51a614
Jordy-Vorle <jordyvandevorle@hotmail.com>
2023-03-16 15:33:53 +0100

    regression analysis

--- src/analysis/regression.R
+++ src/analysis/regression.R
@@ -1,8 +1,13 @@
 # Load the "stats" package
 library(stats)
 
+# load 
+grouped_df2 <- read.csv("grouped_df2.csv")
+View(grouped_df2)
+
 # Perform the multiple regression analysis
 lr_model <- lm(avg_price ~ dummy + room_type, data = grouped_df2)
+summary(lr_model)
 
 # Print the summary of the regression model
 summary(lr_model)
--- src/analysis/starting code.R
+++
@@ -1,5 +0,0 @@
-install.packages("readr")
-library("readr")
-
-download.file("https://github.com/course-dprep/team-assignment-team3/blob/master/src/data-preparation/Joined%26Cleaned_data.R", destfile = "grouped_df2.csv")
-grouped_df2 <- read.csv("grouped_df2.csv")
