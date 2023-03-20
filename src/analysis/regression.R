eec7c1eee4e6ff9f8440c46b72409837eacea4f3
WiebeWeijs <123948939+WiebeWeijs@users.noreply.github.com>
2023-03-20 15:12:06 +0100

    changed the regression.R

--- src/analysis/regression.R
+++ src/analysis/regression.R
@@ -1,13 +1,13 @@
 # Load the "stats" package
 library(stats)
+library(dplyr)
 
 # load 
-grouped_df2 <- read.csv("grouped_df2.csv")
-View(grouped_df2)
+merged_calender_listing <- read.csv("merged.csv")
+View(merged_calender_listing)
 
 # Perform the multiple regression analysis
-lr_model <- lm(avg_price ~ dummy + room_type, data = grouped_df2)
-summary(lr_model)
+lr_model <- lm(avg_price ~ dummy + room_type, data = merged_calender_listing)
 
 # Print the summary of the regression model
-summary(lr_model) 
+summary(lr_model)
