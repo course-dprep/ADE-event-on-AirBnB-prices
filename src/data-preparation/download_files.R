ff376c32dc702f0fd29ad888b34d6b01c458904f
WiebeWeijs <123948939+WiebeWeijs@users.noreply.github.com>
2023-03-27 19:07:11 +0200

    remove prototype

--- src/data-preparation/download_files.R
+++ src/data-preparation/download_files.R
@@ -3,9 +3,6 @@ library(tidyverse)
 library(lubridate)
 library(readr)
 
-prototype = TRUE # set to TRUE for prototyping
-n_max = 1E4
-if (prototype==F) n_max = NULL
 
 # Set the URLs for the CSV files
 url1 <- "http://data.insideairbnb.com/the-netherlands/north-holland/amsterdam/2022-12-05/data/calendar.csv.gz"
@@ -26,10 +23,10 @@ download.file(url3, destfile = filename3)
 download.file(url4, destfile = filename4)
 
 # Read the CSV files into data frames
-data1 <- read_csv(gzfile(filename1), n_max=n_max)
-data2 <- read_csv(gzfile(filename2), n_max=n_max)
-data3 <- read_csv(gzfile(filename3), n_max=n_max)
-data4 <- read_csv(gzfile(filename4), n_max=n_max)
+data1 <- read_csv(gzfile(filename1))
+data2 <- read_csv(gzfile(filename2))
+data3 <- read_csv(gzfile(filename3))
+data4 <- read_csv(gzfile(filename4))
 
 # Merge the data frames into a single data frame
 data <- bind_rows(data1, data2, data3, data4)
@@ -39,10 +36,10 @@ write_csv(data, 'calendar_amsterdam.csv')
 # View(data)
 print('Downloading next set')
 # download and merge csv files Amsterdam 2022
-df1 <- read_csv("http://data.insideairbnb.com/the-netherlands/north-holland/amsterdam/2022-12-05/visualisations/listings.csv", n_max=n_max)
-df2 <- read_csv("http://data.insideairbnb.com/the-netherlands/north-holland/amsterdam/2022-09-07/visualisations/listings.csv", n_max=n_max)
-df3 <- read_csv("http://data.insideairbnb.com/the-netherlands/north-holland/amsterdam/2022-06-05/visualisations/listings.csv", n_max=n_max)
-df4 <- read_csv("http://data.insideairbnb.com/the-netherlands/north-holland/amsterdam/2022-03-08/visualisations/listings.csv", n_max=n_max)
+df1 <- read_csv("http://data.insideairbnb.com/the-netherlands/north-holland/amsterdam/2022-12-05/visualisations/listings.csv")
+df2 <- read_csv("http://data.insideairbnb.com/the-netherlands/north-holland/amsterdam/2022-09-07/visualisations/listings.csv")
+df3 <- read_csv("http://data.insideairbnb.com/the-netherlands/north-holland/amsterdam/2022-06-05/visualisations/listings.csv")
+df4 <- read_csv("http://data.insideairbnb.com/the-netherlands/north-holland/amsterdam/2022-03-08/visualisations/listings.csv")
 
 df <- bind_rows(df1,df2,df3,df4)
 
